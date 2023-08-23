3009fe022b8b4f76480acfe5808cbf7b387df10f
unknown <islom.khurramov@gmail.com>
2023-08-23 20:06:29 +0900

    feat: starting point of project with express

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Web Serverni Boshlash");
+const express = require("express");
+const app = express();
+
+//Mongodb chaqirish
+const db = require("./server").db();
+const mongodb = require("mongodb");
+
+//1
+app.use(express.static("public"));
+app.use("/uploads", express.static(__dirname + "/uploads"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+//2
+
+//3
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4routing
+
+module.exports = app;
