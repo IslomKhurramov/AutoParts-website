55ccdfda7e0031debcca40d682938e3664354edf
IslomKhurramov <islom.khurramov@gmail.com>
2023-09-16 22:55:47 +0900

    feat: create token jwt token and enable cookie parser

--- controller/memberController.js
+++ controller/memberController.js
@@ -1,5 +1,8 @@
 let memberController = module.exports;
 const Member = require("../models/Member");
+const jwt = require("jsonwebtoken");
+const assert = require("assert");
+const Definer = require("../lib/mistake");
 
 memberController.signup = async (req, res) => {
   try {
@@ -7,6 +10,14 @@ memberController.signup = async (req, res) => {
     const data = req.body;
     const member = new Member();
     const new_member = await member.signupData(data);
+
+    //TODO: authenticate based on jwt
+    const token = memberController.createToken(new_member);
+
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
 
     res.json({ state: "succeed", data: new_member });
   } catch (err) {
@@ -22,6 +33,14 @@ memberController.login = async (req, res) => {
     const member = new Member();
     const result = await member.loginData(data);
 
+    //TODO: authenticate based on jwt
+    const token = memberController.createToken(result);
+
+    res.cookie("access_token", token, {
+      maxAge: 6 * 3600 * 1000,
+      httpOnly: true,
+    });
+
     res.json({ state: "succeed", data: result });
   } catch (err) {
     console.log(`ERROR, cont/login`);
@@ -32,4 +51,23 @@ memberController.login = async (req, res) => {
 memberController.logout = (req, res) => {
   console.log("GET cont.logout");
   res.send("logout page");
+};
+
+memberController.createToken = (result) => {
+  try {
+    const upload_data = {
+      _id: result._id,
+      mb_nick: result.mb_nick,
+      mb_type: result.mb_type,
+    };
+
+    const token = jwt.sign(upload_data, process.env.SECRET_TOKEN, {
+      expiresIn: "6h",
+    });
+
+    assert.ok(token, Definer.auth_err2);
+    return token;
+  } catch (err) {
+    throw err;
+  }
 };
