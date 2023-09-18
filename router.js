ef81d3e4af363a61fedbd682fe82d18f0d0ac5ee
IslomKhurramov <islom.khurramov@gmail.com>
2023-09-18 09:54:45 +0900

    feat: develop getArticles API

--- router.js
+++ router.js
@@ -66,4 +66,11 @@ router.get(
   memberController.retrieveAuthMember,
   communityController.getMemberArticles
 );
+
+router.get(
+  "/community/target",
+  memberController.retrieveAuthMember,
+  communityController.getArticles
+);
+
 module.exports = router;
