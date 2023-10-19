3ebbe61c09ac6b64350227e439a93cfbfb4918f6
IslomKhurramov <islom.khurramov@gmail.com>
2023-10-19 23:19:44 +0900

    feat: fixing server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
+// mongoose.set("strictQuery", false);
 
 const connectionString = process.env.MONGO_URL;
 mongoose.connect(
